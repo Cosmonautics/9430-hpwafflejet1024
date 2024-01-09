211fde98a8d58810820813e743fba932380e59ad
Brandon Bontrager <bbontrager89@gmail.com>
2024-01-08 19:04:07 -0800

    Import MAXSwerve template from offseason repo

---
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -0,0 +1,170 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/DriveSubsystem.h"
+
+#include <frc/geometry/Rotation2d.h>
+#include <units/angle.h>
+#include <units/angular_velocity.h>
+#include <units/velocity.h>
+
+#include "Constants.h"
+#include "utils/SwerveUtils.h"
+
+using namespace DriveConstants;
+
+DriveSubsystem::DriveSubsystem()
+    : m_frontLeft{kFrontLeftDrivingCanId, kFrontLeftTurningCanId,
+                  kFrontLeftChassisAngularOffset},
+      m_rearLeft{kRearLeftDrivingCanId, kRearLeftTurningCanId,
+                 kRearLeftChassisAngularOffset},
+      m_frontRight{kFrontRightDrivingCanId, kFrontRightTurningCanId,
+                   kFrontRightChassisAngularOffset},
+      m_rearRight{kRearRightDrivingCanId, kRearRightTurningCanId,
+                  kRearRightChassisAngularOffset},
+      m_odometry{kDriveKinematics,
+                 frc::Rotation2d(units::radian_t{m_gyro.GetAngle()}),
+                 {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+                  m_rearLeft.GetPosition(), m_rearRight.GetPosition()},
+                 frc::Pose2d{}} {}
+
+void DriveSubsystem::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+  m_odometry.Update(frc::Rotation2d(units::radian_t{m_gyro.GetAngle()}),
+                    {m_frontLeft.GetPosition(), m_rearLeft.GetPosition(),
+                     m_frontRight.GetPosition(), m_rearRight.GetPosition()});
+}
+
+void DriveSubsystem::Drive(units::meters_per_second_t xSpeed,
+                           units::meters_per_second_t ySpeed,
+                           units::radians_per_second_t rot, bool fieldRelative,
+                           bool rateLimit) {
+  double xSpeedCommanded;
+  double ySpeedCommanded;
+
+  if (rateLimit) {
+    // Convert XY to polar for rate limiting
+    double inputTranslationDir = atan2(ySpeed.value(), xSpeed.value());
+    double inputTranslationMag =
+        sqrt(pow(xSpeed.value(), 2) + pow(ySpeed.value(), 2));
+
+    // Calculate the direction slew rate based on an estimate of the lateral
+    // acceleration
+    double directionSlewRate;
+    if (m_currentTranslationMag != 0.0) {
+      directionSlewRate =
+          abs(DriveConstants::kDirectionSlewRate / m_currentTranslationMag);
+    } else {
+      directionSlewRate = 500.0;  // some high number that means the slew rate
+                                  // is effectively instantaneous
+    }
+
+    double currentTime = wpi::Now() * 1e-6;
+    double elapsedTime = currentTime - m_prevTime;
+    double angleDif = SwerveUtils::AngleDifference(inputTranslationDir,
+                                                   m_currentTranslationDir);
+    if (angleDif < 0.45 * std::numbers::pi) {
+      m_currentTranslationDir = SwerveUtils::StepTowardsCircular(
+          m_currentTranslationDir, inputTranslationDir,
+          directionSlewRate * elapsedTime);
+      m_currentTranslationMag = m_magLimiter.Calculate(inputTranslationMag);
+    } else if (angleDif > 0.85 * std::numbers::pi) {
+      if (m_currentTranslationMag >
+          1e-4) {  // some small number to avoid floating-point errors with
+                   // equality checking
+        // keep currentTranslationDir unchanged
+        m_currentTranslationMag = m_magLimiter.Calculate(0.0);
+      } else {
+        m_currentTranslationDir =
+            SwerveUtils::WrapAngle(m_currentTranslationDir + std::numbers::pi);
+        m_currentTranslationMag = m_magLimiter.Calculate(inputTranslationMag);
+      }
+    } else {
+      m_currentTranslationDir = SwerveUtils::StepTowardsCircular(
+          m_currentTranslationDir, inputTranslationDir,
+          directionSlewRate * elapsedTime);
+      m_currentTranslationMag = m_magLimiter.Calculate(0.0);
+    }
+    m_prevTime = currentTime;
+
+    xSpeedCommanded = m_currentTranslationMag * cos(m_currentTranslationDir);
+    ySpeedCommanded = m_currentTranslationMag * sin(m_currentTranslationDir);
+    m_currentRotation = m_rotLimiter.Calculate(rot.value());
+
+  } else {
+    xSpeedCommanded = xSpeed.value();
+    ySpeedCommanded = ySpeed.value();
+    m_currentRotation = rot.value();
+  }
+
+  // Convert the commanded speeds into the correct units for the drivetrain
+  units::meters_per_second_t xSpeedDelivered =
+      xSpeedCommanded * DriveConstants::kMaxSpeed;
+  units::meters_per_second_t ySpeedDelivered =
+      ySpeedCommanded * DriveConstants::kMaxSpeed;
+  units::radians_per_second_t rotDelivered =
+      m_currentRotation * DriveConstants::kMaxAngularSpeed;
+
+  auto states = kDriveKinematics.ToSwerveModuleStates(
+      fieldRelative
+          ? frc::ChassisSpeeds::FromFieldRelativeSpeeds(
+                xSpeedDelivered, ySpeedDelivered, rotDelivered,
+                frc::Rotation2d(units::radian_t{m_gyro.GetAngle()}))
+          : frc::ChassisSpeeds{xSpeedDelivered, ySpeedDelivered, rotDelivered});
+
+  kDriveKinematics.DesaturateWheelSpeeds(&states, DriveConstants::kMaxSpeed);
+
+  auto [fl, fr, bl, br] = states;
+
+  m_frontLeft.SetDesiredState(fl);
+  m_frontRight.SetDesiredState(fr);
+  m_rearLeft.SetDesiredState(bl);
+  m_rearRight.SetDesiredState(br);
+}
+
+void DriveSubsystem::SetX() {
+  m_frontLeft.SetDesiredState(
+      frc::SwerveModuleState{0_mps, frc::Rotation2d{45_deg}});
+  m_frontRight.SetDesiredState(
+      frc::SwerveModuleState{0_mps, frc::Rotation2d{-45_deg}});
+  m_rearLeft.SetDesiredState(
+      frc::SwerveModuleState{0_mps, frc::Rotation2d{-45_deg}});
+  m_rearRight.SetDesiredState(
+      frc::SwerveModuleState{0_mps, frc::Rotation2d{45_deg}});
+}
+
+void DriveSubsystem::SetModuleStates(
+    wpi::array<frc::SwerveModuleState, 4> desiredStates) {
+  kDriveKinematics.DesaturateWheelSpeeds(&desiredStates,
+                                         DriveConstants::kMaxSpeed);
+  m_frontLeft.SetDesiredState(desiredStates[0]);
+  m_frontRight.SetDesiredState(desiredStates[1]);
+  m_rearLeft.SetDesiredState(desiredStates[2]);
+  m_rearRight.SetDesiredState(desiredStates[3]);
+}
+
+void DriveSubsystem::ResetEncoders() {
+  m_frontLeft.ResetEncoders();
+  m_rearLeft.ResetEncoders();
+  m_frontRight.ResetEncoders();
+  m_rearRight.ResetEncoders();
+}
+
+units::degree_t DriveSubsystem::GetHeading() const {
+  return frc::Rotation2d(units::radian_t{m_gyro.GetAngle()}).Degrees();
+}
+
+void DriveSubsystem::ZeroHeading() { m_gyro.Reset(); }
+
+double DriveSubsystem::GetTurnRate() { return -m_gyro.GetRate().value(); }
+
+frc::Pose2d DriveSubsystem::GetPose() { return m_odometry.GetPose(); }
+
+void DriveSubsystem::ResetOdometry(frc::Pose2d pose) {
+  m_odometry.ResetPosition(
+      GetHeading(),
+      {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+       m_rearLeft.GetPosition(), m_rearRight.GetPosition()},
+      pose);
+}
---
+++ src/main/include/Constants.h
@@ -0,0 +1,141 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <frc/trajectory/TrapezoidProfile.h>
+#include <rev/CANSparkMax.h>
+#include <units/acceleration.h>
+#include <units/angular_acceleration.h>
+#include <units/angular_velocity.h>
+#include <units/current.h>
+#include <units/length.h>
+#include <units/velocity.h>
+
+#include <numbers>
+
+#pragma once
+
+/**
+ * The Constants header provides a convenient place for teams to hold robot-wide
+ * numerical or bool constants.  This should not be used for any other purpose.
+ *
+ * It is generally a good idea to place constants into subsystem- or
+ * command-specific namespaces within this header, which can then be used where
+ * they are needed.
+ */
+
+namespace DriveConstants {
+// Driving Parameters - Note that these are not the maximum capable speeds of
+// the robot, rather the allowed maximum speeds
+constexpr units::meters_per_second_t kMaxSpeed = 4.8_mps;
+constexpr units::radians_per_second_t kMaxAngularSpeed{2 * std::numbers::pi};
+
+constexpr double kDirectionSlewRate = 1.2;   // radians per second
+constexpr double kMagnitudeSlewRate = 1.8;   // percent per second (1 = 100%)
+constexpr double kRotationalSlewRate = 2.0;  // percent per second (1 = 100%)
+
+// Chassis configuration
+constexpr units::meter_t kTrackWidth =
+    0.6731_m;  // Distance between centers of right and left wheels on robot
+constexpr units::meter_t kWheelBase =
+    0.6731_m;  // Distance between centers of front and back wheels on robot
+
+// Angular offsets of the modules relative to the chassis in radians
+constexpr double kFrontLeftChassisAngularOffset = -std::numbers::pi / 2;
+constexpr double kFrontRightChassisAngularOffset = 0;
+constexpr double kRearLeftChassisAngularOffset = std::numbers::pi;
+constexpr double kRearRightChassisAngularOffset = std::numbers::pi / 2;
+
+// SPARK MAX CAN IDs
+constexpr int kFrontLeftDrivingCanId = 2;
+constexpr int kRearLeftDrivingCanId = 6;
+constexpr int kFrontRightDrivingCanId = 4;
+constexpr int kRearRightDrivingCanId = 8;
+
+constexpr int kFrontLeftTurningCanId = 1;
+constexpr int kRearLeftTurningCanId = 5;
+constexpr int kFrontRightTurningCanId = 3;
+constexpr int kRearRightTurningCanId = 7;
+}  // namespace DriveConstants
+
+namespace ModuleConstants {
+// Invert the turning encoder, since the output shaft rotates in the opposite
+// direction of the steering motor in the MAXSwerve Module.
+constexpr bool kTurningEncoderInverted = true;
+
+// The MAXSwerve module can be configured with one of three pinion gears: 12T,
+// 13T, or 14T. This changes the drive speed of the module (a pinion gear with
+// more teeth will result in a robot that drives faster).
+constexpr int kDrivingMotorPinionTeeth = 14;
+
+// Calculations required for driving motor conversion factors and feed forward
+constexpr double kDrivingMotorFreeSpeedRps =
+    5676.0 / 60;  // NEO free speed is 5676 RPM
+constexpr units::meter_t kWheelDiameter = 0.0762_m;
+constexpr units::meter_t kWheelCircumference =
+    kWheelDiameter * std::numbers::pi;
+// 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+// teeth on the bevel pinion
+constexpr double kDrivingMotorReduction =
+    (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
+constexpr double kDriveWheelFreeSpeedRps =
+    (kDrivingMotorFreeSpeedRps * kWheelCircumference.value()) /
+    kDrivingMotorReduction;
+
+constexpr double kDrivingEncoderPositionFactor =
+    (kWheelDiameter.value() * std::numbers::pi) /
+    kDrivingMotorReduction;  // meters
+constexpr double kDrivingEncoderVelocityFactor =
+    ((kWheelDiameter.value() * std::numbers::pi) / kDrivingMotorReduction) /
+    60.0;  // meters per second
+
+constexpr double kTurningEncoderPositionFactor =
+    (2 * std::numbers::pi);  // radians
+constexpr double kTurningEncoderVelocityFactor =
+    (2 * std::numbers::pi) / 60.0;  // radians per second
+
+constexpr units::radian_t kTurningEncoderPositionPIDMinInput = 0_rad;
+constexpr units::radian_t kTurningEncoderPositionPIDMaxInput =
+    units::radian_t{kTurningEncoderPositionFactor};
+
+constexpr double kDrivingP = 0.04;
+constexpr double kDrivingI = 0;
+constexpr double kDrivingD = 0;
+constexpr double kDrivingFF = (1 / kDriveWheelFreeSpeedRps);
+constexpr double kDrivingMinOutput = -1;
+constexpr double kDrivingMaxOutput = 1;
+
+constexpr double kTurningP = 1;
+constexpr double kTurningI = 0;
+constexpr double kTurningD = 0;
+constexpr double kTurningFF = 0;
+constexpr double kTurningMinOutput = -1;
+constexpr double kTurningMaxOutput = 1;
+
+constexpr rev::CANSparkMax::IdleMode kDrivingMotorIdleMode =
+    rev::CANSparkMax::IdleMode::kBrake;
+constexpr rev::CANSparkMax::IdleMode kTurningMotorIdleMode =
+    rev::CANSparkMax::IdleMode::kBrake;
+
+constexpr units::ampere_t kDrivingMotorCurrentLimit = 50_A;
+constexpr units::ampere_t kTurningMotorCurrentLimit = 20_A;
+}  // namespace ModuleConstants
+
+namespace AutoConstants {
+constexpr auto kMaxSpeed = 3_mps;
+constexpr auto kMaxAcceleration = 3_mps_sq;
+constexpr auto kMaxAngularSpeed = 3.142_rad_per_s;
+constexpr auto kMaxAngularAcceleration = 3.142_rad_per_s_sq;
+
+constexpr double kPXController = 0.5;
+constexpr double kPYController = 0.5;
+constexpr double kPThetaController = 0.5;
+
+extern const frc::TrapezoidProfile<units::radians>::Constraints
+    kThetaControllerConstraints;
+}  // namespace AutoConstants
+
+namespace OIConstants {
+constexpr int kDriverControllerPort = 0;
+constexpr double kDriveDeadband = 0.05;
+}  // namespace OIConstants
