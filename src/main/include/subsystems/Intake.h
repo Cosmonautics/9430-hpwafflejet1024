ac23ae4e7ee9aa215b41ad8571685f3ddc630968
Brandon Bontrager <bbontrager89@gmail.com>
2024-01-22 19:40:21 -0800

    modify intake and shooter comments for clarity

--- src/main/include/subsystems/Intake.h
+++ src/main/include/subsystems/Intake.h
@@ -1,4 +1,9 @@
 // 2 mototers 
-    // 1 for pivit angel
-    // 1 motor for rollers
+    // Possible design A:
+        // 1 for pivot angle
+        // 1 motor for rollers
+    // OR design B:
+        // 2 for pivot angle
+        // 2 for rollers
+    
 // 1 throughbore incoder
--- src/main/include/subsystems/Shooter.h
+++ src/main/include/subsystems/Shooter.h
@@ -1,3 +1,3 @@
-// one pivit motoer
-// one roller motoer 
-// two shooter motoer 
+// one pivot motor - shooter manipulator
+// one roller motor - for intake part (will function at shooting out at full speed)
+// two shooter motor - slow, "fly" wheels
