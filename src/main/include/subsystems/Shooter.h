10e318fd9d6961e28944935118480e766b8acaa2
salut-hi <151594270+salut-hi@users.noreply.github.com>
2024-01-27 14:15:32 -0800

    Update Shooter.h functions from Drive Subsystem.h

--- src/main/include/subsystems/Shooter.h
+++ src/main/include/subsystems/Shooter.h
@@ -1,3 +1,8 @@
 // one pivot motor - shooter manipulator
 // one roller motor - for intake part (will function at shooting out at full speed)
 // two shooter motor - slow, "fly" wheels
+
+
+
+
+void ShootMotors(bool isPressed, double speed);
