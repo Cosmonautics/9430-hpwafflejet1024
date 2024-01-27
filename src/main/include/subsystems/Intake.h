c38d7c59583060ad61b3a16fbee63042a470a486
salut-hi <151594270+salut-hi@users.noreply.github.com>
2024-01-27 13:54:18 -0800

    update 2 new intake motor

--- src/main/include/subsystems/Intake.h
+++ src/main/include/subsystems/Intake.h
@@ -1,7 +1,8 @@
 // 2 motors 
     //Design:
         // 1 for pivot angle
-
+  rev::CANSparkMax m_intakePivotMax;
         // 1 motor for rollers
+  rev::CANSparkFlex m_intakeRollersFlex;
 
 // 1 throughbore encoder
