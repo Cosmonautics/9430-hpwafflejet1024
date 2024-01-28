111bd69979803496591dbad06117803eccdfb06e
Brandon Bontrager <bbontrager89@gmail.com>
2024-01-27 18:39:48 -0800

    Add shooterPickupNote and shooterDropNote to source file

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -17,3 +17,27 @@ void Shooter::ShootMotors(bool isPressed, double speed) {
     m_shooterMotorRight.Set(0);
   }
 }
+
+void Shooter::ShooterDropNote(bool isPressed, double speed) {
+  double dropSpeed = 0.10;  // drop positive speed
+
+  if (isPressed) {
+    m_shooterMotorLeft.Set(-dropSpeed);
+    m_shooterMotorRight.Set(dropSpeed);
+  } else {
+    m_shooterMotorLeft.Set(0);
+    m_shooterMotorRight.Set(0);
+  }
+}
+
+void Shooter::ShooterPickUpNote(bool isPressed, double speed) {
+  double pickUpSpeed = -0.10;  // pick up is negative speed
+
+  if (isPressed) {
+    m_shooterMotorLeft.Set(-pickUpSpeed);
+    m_shooterMotorRight.Set(pickUpSpeed);
+  } else {
+    m_shooterMotorLeft.Set(0);
+    m_shooterMotorRight.Set(0);
+  }
+}
