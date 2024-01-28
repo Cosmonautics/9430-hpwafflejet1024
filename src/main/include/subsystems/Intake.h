985b186edd71c8e7bce3d057fa5374c9e128a073
Brandon Bontrager <bbontrager89@gmail.com>
2024-01-27 16:01:33 -0800

    create new intake object

--- src/main/include/subsystems/Intake.h
+++ src/main/include/subsystems/Intake.h
@@ -1,11 +1,18 @@
 #include "Elevator.h"
 #include "Shooter.h"
 
-// 2 motors 
-    //Design:
-        // 1 for pivot angle
+class Intake : public frc2::SubsystemBase {
+ public:
+  Intake();
+
+  frc2::Command* GetAutonomousCommand();
+
+ private:
+  // 2 motors
+  // Design:
+  // 1 for pivot angle
   rev::CANSparkMax m_intakePivotMax;
-        // 1 motor for rollers
+  // 1 motor for rollers
   rev::CANSparkFlex m_intakeRollersFlex;
 
   void PickUpNote(bool isPressed, double speed);
@@ -13,5 +20,10 @@
   void DropNote(bool isPressed, double speed);
 
   void ControlIntakeMotors(bool isPressed, double speed);
+  // The chooser for the autonomous routines
+  frc::SendableChooser<frc2::Command*> m_chooser;
 
-// 1 throughbore encoder
+  void ConfigureButtonBindings();
+
+  // 1 throughbore encoder
+};
