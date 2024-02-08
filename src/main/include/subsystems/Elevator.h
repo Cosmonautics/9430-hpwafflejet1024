4740fb82d033a49bc7a7ddaf328edb06fe6e4ea0
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-02-08 00:27:14 -0800

    Add MoveToRelativePosition to elevator

--- src/main/include/subsystems/Elevator.h
+++ src/main/include/subsystems/Elevator.h
@@ -20,6 +20,7 @@ class Elevator : public frc2::Subsystem {
   void Periodic() override;
   void MoveToPosition(double position);
   bool AtTargetPosition();
+  void MoveToRelativePosition(double position);
   
   // Helper methods
   void ConfigureMotors();
@@ -27,36 +28,39 @@ class Elevator : public frc2::Subsystem {
   double ConvertEncoderUnitsToInches(double units);
   void UpdatePosition();
   void Move(double speed);
-  rev::SparkMaxAbsoluteEncoder GetkElevatorThroughBoreEncoder(); // these functions are needed to get private class attributes
-  rev::SparkMaxPIDController GetkElevatorPIDController(); // these functions are needed to get private class attributes 
+  rev::SparkMaxAbsoluteEncoder
+  GetkElevatorThroughBoreEncoder();  // these functions are needed to get
+                                     // private class attributes
+  rev::SparkMaxPIDController
+  GetkElevatorPIDController();  // these functions are needed to get private
+                                // class attributes
 
  private:
-  rev::CANSparkMax m_ElevatorMotorLeft{
-      kElevatorLeftCanId, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_ElevatorMotorLeft{kElevatorLeftCanId,
+                                       rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax m_ElevatorMotorRight{
       kElevatorRightCanId, rev::CANSparkMax::MotorType::kBrushless};
   // Changed to SparkMaxAbsoluteEncoder for absolute position measurement
   rev::SparkMaxAbsoluteEncoder m_ElevatorEncoder =
-      m_ElevatorMotorLeft.GetAbsoluteEncoder(rev::SparkAbsoluteEncoder::Type::kDutyCycle);
+      m_ElevatorMotorLeft.GetAbsoluteEncoder(
+          rev::SparkAbsoluteEncoder::Type::kDutyCycle);
   // Define CANPIDController for direct control through SparkMax
-  rev::SparkMaxPIDController m_pidController = 
+  rev::SparkMaxPIDController m_pidController =
       m_ElevatorMotorLeft.GetPIDController();
   double currentPositionInches = 0;  // Current elevator position in inches
   double targetPositionInches = 0;   // Target elevator position in inches
   // trigger: xbox controller button
 
-
-
-// go down
-    // trigger: xbox controller button
+  // go down
+  // trigger: xbox controller button
 
   // throughbore encoder
   // 2 motors, one inverted
-	
- /**
-* An alternate encoder object is constructed using the GetAlternateEncoder()
-* method on an existing CANSparkMax object. If using a REV Through Bore
-* Encoder, the type should be set to quadrature and the counts per
-* revolution set to 8192
-*/
+
+  /**
+   * An alternate encoder object is constructed using the GetAlternateEncoder()
+   * method on an existing CANSparkMax object. If using a REV Through Bore
+   * Encoder, the type should be set to quadrature and the counts per
+   * revolution set to 8192
+   */
 };
