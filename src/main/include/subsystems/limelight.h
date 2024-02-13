42d8353d2c4e5e34f35b85d7aec256a91b6b3b34
William-Ramey <670251@salemkeizer.org>
2024-02-12 18:41:42 -0800

    added working networktables and vaules for hight

--- src/main/include/subsystems/limelight.h
+++ src/main/include/subsystems/limelight.h
@@ -1,12 +1,11 @@
-#include <Robot.h>
-#include <frc/smartdashboard/SmartDashboard.h>
-#include "rev/CANSparkMax.h"
-#include "frc/GenericHID.h"
-#include <frc/drive/differentialdrive.h>
 #include "frc/smartdashboard/Smartdashboard.h"
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
 #include "networktables/NetworkTableEntry.h"
 #include "networktables/NetworkTableValue.h"
+#include "frc/smartdashboard/SmartDashboard.h"
+#include "networktables/NetworkTableInstance.h"
+#include <memory>
+
 
 nt::NetworkTableInstance inst = nt::NetworkTableInstance::GetDefault();
