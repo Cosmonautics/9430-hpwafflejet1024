9afab9abcfc60f991da95d9b174201e0a762b01a
FRCteam9430 <frc.9430@outlook.com>
2024-02-20 14:48:38 -0500

    Commit before pushing

--- src/main/cpp/commands/MoveToFloorIntakePositionCommand.cpp
+++ src/main/cpp/commands/MoveToFloorIntakePositionCommand.cpp
@@ -1,29 +1,19 @@
 #include "commands/MoveToFloorIntakePositionCommand.h"
 
-MoveToFloorIntakePositionCommand::MoveToFloorIntakePositionCommand(Elevator* elevatorSubsystem, Shooter* shooterSubsystem, Intake intakeSubsystem)
+MoveToFloorIntakePositionCommand::MoveToFloorIntakePositionCommand(Elevator* elevatorSubsystem, Shooter* shooterSubsystem, Intake* intakeSubsystem)
 : m_elevatorSubsystem(elevatorSubsystem), m_shooterSubsystem(shooterSubsystem), m_intakeSubsystem(intakeSubsystem) {
     AddRequirements({elevatorSubsystem, shooterSubsystem, intakeSubsystem});
 }
 
+
 void MoveToFloorIntakePositionCommand::Initialize() {
     m_elevatorSubsystem->MoveToPosition(ElevatorConstants::kFloorIntakePositionInches);
-}
-
-bool MoveToFloorIntakePositionCommand::IsFinished() {
-    return m_elevatorSubsystem->AtTargetPosition();
-}
-
-
-
-PivotShooterToFloorIntakePositionCommand::PivotShooterToFloorIntakePositionCommand(Shooter* shooterSubsystem)
-: m_shooterSubsystem(shooterSubsystem) {
-    AddRequirements({shooterSubsystem});
-}
-
-void PivotShooterToFloorIntakePositionCommand::Initialize() {
     m_shooterSubsystem->PivotToSetPoint(ShooterConstants::kFloorIntakePositionDegrees);
 }
 
-bool PivotShooterToFloorIntakePositionCommand::IsFinished() {
+void MoveToFloorIntakePositionCommand::Execute() {}
+
+bool MoveToFloorIntakePositionCommand::IsFinished() {
+    return m_elevatorSubsystem->AtTargetPosition();
     return m_shooterSubsystem->IsAtSetPoint();
 }
