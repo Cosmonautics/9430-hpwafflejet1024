44a6420e92d2fd1c1bb6959999fcd747e961e531
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-02-24 23:09:01 -0800

    add DoNoteIntakeActionCommand

---
+++ src/main/include/commands/DoNoteIntakeActionCommand.h
@@ -0,0 +1,25 @@
+#ifndef COMMAND_TEMPLATE_H
+#define COMMAND_TEMPLATE_H // rename these to the name of the command in all caps followed by _H
+
+#include <frc2/command/Command.h>
+#include <frc2/command/CommandHelper.h>
+#include <frc2/command/WaitCommand.h>
+#include "subsystems/Conveyor.h" // import all relevant subsystems 
+#include "subsystems/Shooter.h" // import all relevant subsystems 
+#include "subsystems/Intake.h" // import all relevant subsystems 
+
+class DoNoteIntakeActionCommand : public frc2::CommandHelper<frc2::Command, DoNoteIntakeActionCommand> {
+public:
+    DoNoteIntakeActionCommand(Conveyor* conveyorSubsystem, Shooter* shooterSubsystem, Intake* intakeSubsystem); // Put in all relevant subsystems
+
+    void Initialize() override;
+    void Execute() override;
+    bool IsFinished() override;
+
+private:
+    Conveyor* m_conveyorSubsystem; // Put all relevant subsystems here as pointers
+    Shooter* m_shooterSubsystem;
+    Intake* m_intakeSubsystem;
+};
+
+#endif // COMMAND_TEMPLATE_H // (replace this with the name of the command name)
