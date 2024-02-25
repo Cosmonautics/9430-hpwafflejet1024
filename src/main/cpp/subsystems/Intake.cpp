b0e696c069f3b2e8a9733f531e518cb99f238596
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-02-25 14:40:30 -0800

    invert intake motor

--- src/main/cpp/subsystems/Intake.cpp
+++ src/main/cpp/subsystems/Intake.cpp
@@ -19,7 +19,7 @@ elevator postion and shooter position remain defult to transit positon
 
 Intake::Intake() {
   m_intakePivotMotor.RestoreFactoryDefaults();
-  m_intakePivotMotor.SetInverted(false);
+  m_intakePivotMotor.SetInverted(true);
   m_pidController.SetOutputRange(-1.0, 1.0);
   m_pidController.SetFeedbackDevice(m_intakePivotAbsoluteEncoder);
   m_pidController.SetP(IntakeConstants::kP);
