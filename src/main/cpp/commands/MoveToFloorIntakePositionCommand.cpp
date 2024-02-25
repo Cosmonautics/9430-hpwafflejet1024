d5b57efdd7c134cfba988368ae59405f009e6a33
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-02-24 21:25:31 -0800

    update floor intake command

--- src/main/cpp/commands/MoveToFloorIntakePositionCommand.cpp
+++ src/main/cpp/commands/MoveToFloorIntakePositionCommand.cpp
@@ -12,13 +12,16 @@ MoveToFloorIntakePositionCommand::MoveToFloorIntakePositionCommand(
 void MoveToFloorIntakePositionCommand::Initialize() {}
 
 void MoveToFloorIntakePositionCommand::Execute() {
-  m_elevatorSubsystem->MoveToPosition(
-      PositionConstants::kElevatorTransitPosition);
   m_shooterSubsystem->PivotToSetPoint(
       PositionConstants::kShooterTransitPosition);
+  frc2::WaitCommand(1_s).Schedule();
+  m_elevatorSubsystem->MoveToPosition(
+      PositionConstants::kElevatorTransitPosition);
   m_intakeSubsystem->PivotToAngle(PositionConstants::kIntakeFloorPosition);
 }
 
 bool MoveToFloorIntakePositionCommand::IsFinished() {
-  return m_elevatorSubsystem->AtTargetPosition() && m_shooterSubsystem->IsAtSetPoint() && m_intakeSubsystem->IsAtSetPoint();
+  return m_elevatorSubsystem->AtTargetPosition() &&
+         m_shooterSubsystem->IsAtSetPoint() &&
+         m_intakeSubsystem->IsAtSetPoint();
 }
