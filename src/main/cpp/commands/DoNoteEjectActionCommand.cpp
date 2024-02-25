59130f8405f2d38b57b81910b013f168962d958f
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-02-25 13:27:29 -0800

    update donoteejectaction to return true

--- src/main/cpp/commands/DoNoteEjectActionCommand.cpp
+++ src/main/cpp/commands/DoNoteEjectActionCommand.cpp
@@ -16,12 +16,8 @@ void DoNoteEjectActionCommand::Execute() {
   m_conveyorSubsystem->Reverse();
   m_shooterSubsystem->ShooterDropNote(true, 0.10);
   m_shooterSubsystem->MoveFeeder(-1);
-  frc2::WaitCommand(2_s).Schedule();
-  m_conveyorSubsystem->Stop();
-  m_shooterSubsystem->StopMotors();
 }
 
 bool DoNoteEjectActionCommand::IsFinished() {
-  // Return code here
-  // return ...;
+  return true;
 }
