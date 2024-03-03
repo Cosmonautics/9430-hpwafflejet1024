f69be4b4b08ed71a804a3d7d68c8179a42809318
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-03-02 16:14:55 -0800

    add experimental move to note intake pos on

--- src/main/include/commands/DoNoteIntakeActionCommand.h
+++ src/main/include/commands/DoNoteIntakeActionCommand.h
@@ -1,26 +1,35 @@
 #ifndef DO_NOTE_INTAKE_COMMAND_H
-#define DO_NOTE_INTAKE_COMMAND_H // rename these to the name of the command in all caps followed by _H
+#define DO_NOTE_INTAKE_COMMAND_H  // rename these to the name of the command in
+                                  // all caps followed by _H
 
 #include <frc2/command/Command.h>
 #include <frc2/command/CommandHelper.h>
 #include <frc2/command/WaitCommand.h>
-#include "subsystems/Conveyor.h" // import all relevant subsystems 
-#include "subsystems/Shooter.h" // import all relevant subsystems 
-#include "subsystems/Intake.h" // import all relevant subsystems 
 
-class DoNoteIntakeActionCommand : public frc2::CommandHelper<frc2::Command, DoNoteIntakeActionCommand> {
-public:
-    DoNoteIntakeActionCommand(Conveyor* conveyorSubsystem, Shooter* shooterSubsystem, Intake* intakeSubsystem); // Put in all relevant subsystems
+#include "subsystems/Conveyor.h"  // import all relevant subsystems
+#include "subsystems/Elevator.h"  // import all relevant subsystems
+#include "subsystems/Intake.h"    // import all relevant subsystems
+#include "subsystems/Shooter.h"   // import all relevant subsystems
+class DoNoteIntakeActionCommand
+    : public frc2::CommandHelper<frc2::Command, DoNoteIntakeActionCommand> {
+ public:
+  DoNoteIntakeActionCommand(
+      Conveyor* conveyorSubsystem, Shooter* shooterSubsystem,
+      Intake* intakeSubsystem,
+      Elevator* elevatorSubsystem);  // Put in all relevant subsystems
 
-    void Initialize() override;
-    void Execute() override;
-    bool IsFinished() override;
+  void Initialize() override;
+  void Execute() override;
+  bool IsFinished() override;
 
-private:
-    Conveyor* m_conveyorSubsystem; // Put all relevant subsystems here as pointers
-    Shooter* m_shooterSubsystem;
-    Intake* m_intakeSubsystem;
-    bool cmdFinished;
+ private:
+  Conveyor*
+      m_conveyorSubsystem;  // Put all relevant subsystems here as pointers
+  Shooter* m_shooterSubsystem;
+  Intake* m_intakeSubsystem;
+  Elevator* m_elevatorSubsystem;
+  bool cmdFinished;
 };
 
-#endif // DO_NOTE_INTAKE_COMMAND_H // (replace this with the name of the command name)
+#endif  // DO_NOTE_INTAKE_COMMAND_H // (replace this with the name of the
+        // command name)
