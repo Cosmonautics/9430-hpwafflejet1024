5f24ac158d609e9877d2552d4e16eb67f6bd770f
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-03-04 17:08:10 -0800

    Revert "add experimental move to note intake pos on"
    This reverts commit f69be4b4b08ed71a804a3d7d68c8179a42809318.

--- src/main/cpp/commands/DoNoteIntakeActionCommand.cpp
+++ src/main/cpp/commands/DoNoteIntakeActionCommand.cpp
@@ -5,39 +5,26 @@ DoNoteIntakeActionCommand::DoNoteIntakeActionCommand(  // Find and Replace
                                                        // the name of the
                                                        // command
     Conveyor* conveyorSubsystem, Shooter* shooterSubsystem,
-    Intake* intakeSubsystem,
-    Elevator*
-        elevatorSubsystem)  // add all the relevant subsystems to constructor
+    Intake* intakeSubsystem)  // add all the relevant subsystems to constructor
     : m_conveyorSubsystem(conveyorSubsystem),
       m_shooterSubsystem(shooterSubsystem),
-      m_intakeSubsystem(intakeSubsystem),
-      m_elevatorSubsystem(elevatorSubsystem) {
-  AddRequirements({conveyorSubsystem, shooterSubsystem, intakeSubsystem,
-                   elevatorSubsystem});
+      m_intakeSubsystem(intakeSubsystem) {
+  AddRequirements({conveyorSubsystem, shooterSubsystem, intakeSubsystem});
 }
 
 void DoNoteIntakeActionCommand::Initialize() {
-  cmdFinished = false;  // tests should go here to set target states for
-                        // completion to return true
+  cmdFinished = false; // tests should go here to set target states for completion to return true
 }
 
 void DoNoteIntakeActionCommand::Execute() {
-  m_shooterSubsystem->InvertMotor(true);
-  // ENTIRELY EXPERIMENTAL
-  m_shooterSubsystem->PivotToSetPoint(
-      PositionConstants::kShooterTransitPosition);
-  m_elevatorSubsystem->MoveToPosition(
-      PositionConstants::kElevatorTransitPosition, false);
-  // END --
+   m_shooterSubsystem->InvertMotor(true);
   m_intakeSubsystem->IntakePickUpNote(true, 1);
   m_conveyorSubsystem->Forward();
   m_shooterSubsystem->ShooterPickUpNote(true, 0.30);
   m_shooterSubsystem->MoveFeeder(1);
-  cmdFinished = true;  // should only conditionally set to true if and only if
-                       // tests for motor states return true
+  cmdFinished = true; // should only conditionally set to true if and only if tests for motor states return true
 }
 
-bool DoNoteIntakeActionCommand::IsFinished() {
-  return cmdFinished;  // finished logic should ensure test conditions pass (use
-                       // motor position/motor state methods)
+bool DoNoteIntakeActionCommand::IsFinished() { 
+  return cmdFinished; // finished logic should ensure test conditions pass (use motor position/motor state methods)
 }
