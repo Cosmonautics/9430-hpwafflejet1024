3f6e5d2772d66b3f156366e5f6e1779a19678fa2
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-03-21 22:57:52 -0700

    update auto with test

--- src/main/cpp/commands2/autos/Autos.cpp
+++ src/main/cpp/commands2/autos/Autos.cpp
@@ -19,15 +19,16 @@ frc2::CommandPtr autos::GetAndShootFirstThree(Elevator* elevatorSubsystem,
       DoSpeakerScoreCommand(elevatorSubsystem, shooterSubsystem, driveSubsystem,
                             limelightSubsystem)
           .ToPtr(),
-      frc2::WaitCommand(1.2_s).ToPtr(),
+      frc2::WaitCommand(0.2_s).ToPtr(),
       frc2::InstantCommand(
           [driveSubsystem]() {
             driveSubsystem->ResetOdometry(
                 pathplanner::PathPlannerAuto::getStartingPoseFromAutoFile(
                     "GetAndShootFirstThree"));
           },
-          {})
+          {driveSubsystem})
           .ToPtr(),
+      frc2::WaitCommand(0.2_s).ToPtr(),
       pathplanner::PathPlannerAuto("GetAndShootFirstThree").ToPtr());
 }
 frc2::CommandPtr autos::ThreeNoteAuto(Elevator* elevatorSubsystem,
@@ -45,30 +46,31 @@ frc2::CommandPtr autos::ThreeNoteAuto(Elevator* elevatorSubsystem,
                 pathplanner::PathPlannerAuto::getStartingPoseFromAutoFile(
                     "ThreeNoteAuto"));
           },
-          {})
+          {driveSubsystem})
           .ToPtr(),
+      frc2::WaitCommand(0.2_s).ToPtr(),
       pathplanner::PathPlannerAuto("ThreeNoteAuto").ToPtr(),
       DoSpeakerScoreCommand(elevatorSubsystem, shooterSubsystem, driveSubsystem,
                             limelightSubsystem)
           .ToPtr());
 }
 frc2::CommandPtr autos::OneNoteAutoOnSteroids(Elevator* elevatorSubsystem,
-                                      Shooter* shooterSubsystem,
-                                      DriveSubsystem* driveSubsystem,
-                                      Limelight* limelightSubsystem) {
+                                              Shooter* shooterSubsystem,
+                                              DriveSubsystem* driveSubsystem,
+                                              Limelight* limelightSubsystem) {
   return frc2::cmd::Sequence(
       DoSpeakerScoreCommand(elevatorSubsystem, shooterSubsystem, driveSubsystem,
                             limelightSubsystem)
           .ToPtr(),
-      frc2::WaitCommand(0.2_s).ToPtr(),
       frc2::InstantCommand(
           [driveSubsystem]() {
             driveSubsystem->ResetOdometry(
                 pathplanner::PathPlannerAuto::getStartingPoseFromAutoFile(
                     "OneNoteAutoOnSteroids"));
           },
-          {})
+          {driveSubsystem})
           .ToPtr(),
+      frc2::WaitCommand(0.2_s).ToPtr(),
       pathplanner::PathPlannerAuto("OneNoteAutoOnSteroids").ToPtr(),
       DoSpeakerScoreCommand(elevatorSubsystem, shooterSubsystem, driveSubsystem,
                             limelightSubsystem)
