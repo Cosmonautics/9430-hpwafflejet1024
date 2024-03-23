490b0ad9af93bc56df5840487fc6b8fee18c43e4
Auth-Xero <64921040+Auth-Xero@users.noreply.github.com>
2024-03-22 18:42:47 -0700

    Revert "update shooter pos"
    This reverts commit 7866e9d53bb59bd879e8af870aee9d55bdc67691.

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -34,7 +34,7 @@ Shooter::Shooter() {
 
 void Shooter::InitializeDistanceAngleLookup() {
   // Populate the distance-angle lookup table with real data
-  distanceAngleLookup = {{36.0, 0.885},  {60.0, 0.821},  {84.0, 0.794},
+  distanceAngleLookup = {{36.0, 0.846},  {60.0, 0.817},  {84.0, 0.794},
                          {108.0, 0.777}, {132.0, 0.77}, {156.0, 0.763}};
 }
 
